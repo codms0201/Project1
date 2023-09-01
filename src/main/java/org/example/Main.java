724ba26eb133745ed0380ab4989791ee5c46e50f
nana-Kwon <chanyeol0615@naver.com>
2023-09-01 16:58:17 +0900

    first upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        Student s1 = new Student ("21", "che");
+        Student s1 = new Student ("22", "che");
         System.out.print(s1.toString());
 
     }

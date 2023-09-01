0abbf317504fc5aa6b0f19843a3541df0819030c
nana-Kwon <chanyeol0615@naver.com>
2023-09-01 16:54:38 +0900

    first upload

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Student {
+    String id;
+    String name;
+
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return id + name;
+
+    }
+}
